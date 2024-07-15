c9509f5a39731876f15df8359a610a7921a9353f
JensiGhadiya <jensighadiya24@gmail.com>
2024-07-15 15:03:17 +0530

    add courses & theme

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,10 +6,13 @@ import Courses from './courses/Courses';
 function App() {
   return (
     <>
-      <Routes>
+    <div  className='dark:bg-slate-900 dark:text-white'>
+    <Routes >
         <Route path="/" element={<Home />} />
         <Route path="/course" element={<Courses />} />
       </Routes>
+    </div>
+  
     </>
   );
 }
--- Frontend/src/courses/Courses.jsx
+++ Frontend/src/courses/Courses.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import Footer from '../components/Footer'
 import Course from '../components/Course'
 import Navbar from '../components/Navbar'
-import list from '../../public/list.json'
+
 
 function Courses() {
-    console.log(list);
+
   return (
     <>
     <Navbar/>
-    <div className='min-h-screen'>  <Course/></div>
+    <div className='min-h-screen'> 
+       <Course/></div>
     <Footer/>
     </>
   )
